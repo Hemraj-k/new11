ea36c59b30b7a2cc3ec7f449c20eab828f1ba3af
Hemraj-k <83349808+Hemraj-k@users.noreply.github.com>
2022-10-22 20:22:38 +0530

    Initial commit

---
+++ src/MapContainer.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-perf/jsx-no-new-function-as-prop */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react-perf/jsx-no-new-object-as-prop */
+/* eslint-disable no-unused-vars */
+import React, { Fragment } from "react";
+import Map2 from "./Map2.js";
+
+const MapContainer = ({ shownPin }) => {
+  return <Map2 />;
+};
+
+export default MapContainer;
